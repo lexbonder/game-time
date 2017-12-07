973556f2583c31cc4eb14053956f8e18e229f4be
Alex Bonder <lexbonder@gmail.com>
2017-12-06 23:22:01 -0700

    Finish testing

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,33 +1,72 @@
 const {assert} = require('chai');
 const Game = require('../lib/Game.js')
+const LilyPad = require('../lib/LilyPad.js');
+const Log = require('../lib/Log.js');
+const Turtle = require('../lib/Turtle.js');
+const Vehicle = require('../lib/Vehicle.js');
+
+global.document = {
+  getElementById: function (){},
+  querySelector: function () {
+    return {
+      removeAttribute: function() {},
+      setAttribute: function() {
+      }
+    };
+  }
+};
+
+const canvas = {
+  getContext: function() {}
+};
+
+let game;
 
 describe('Game', function() {
+  beforeEach(function() {
+    game = new Game(canvas)
+  })
   it('Should be a function', function() {
     assert.isFunction(Game);
   })
 
-  it.skip('Should create an instance of game', function() {
-    let game = new Game();
-    
+  it('Should create an instance of game', function() {
     assert.isObject(game);
   })
+
+  it('Should be able to clear the landed lilypads', function() {
+    let lilyPad = new LilyPad(10,10,10,10)
+    game.lilyArray.push(lilyPad)
+
+    assert.equal(lilyPad.color, 'transparent');
+    lilyPad.color = 'lightgreen';
+    assert.equal(lilyPad.color, 'lightgreen');
+    game.clearLilys();
+    assert.equal(lilyPad.color, 'transparent');
+  })
+
+  it('Should increase the speed of logs, turtles, and cars', function() {
+    let log = new Log(10,10,10,10,10,'brown');
+    let leftCar = new Vehicle(10,10,10,10,10,'red');
+    let rightCar = new Vehicle(10,10,10,10,-10,'red');
+    let turtle = new Turtle(10,10,10,10,-10,'green');
+
+    game.logs.push(log) 
+    game.laneLeft.push(leftCar)
+    game.laneRight.push(rightCar)
+    game.turtles.push(turtle)
+
+    assert.equal(log.dx, 10)
+    assert.equal(leftCar.dx, 10)
+    assert.equal(rightCar.dx, -10)
+    assert.equal(turtle.dx, -10)
+
+    game.speedUp()
+
+    assert.equal(log.dx, 10.2)
+    assert.equal(leftCar.dx, 10.2)
+    assert.equal(rightCar.dx, -10.2)
+    assert.equal(turtle.dx, -10.2)
+
+  })
 })
-
-// // Needs Game Constructor //
-//   it.skip('Should increase the level if 5 lilyPads are landed on', function() {
-//     let lilyArray = [];
-//     for ( let i = 0; i < 5; i ++ ) {
-//       lilyArray.push(new LilyPad(10,10,10,10))
-//     };
-
-//     assert.equal(game.level, 1)
-
-//     lilyArray.forEach( lilyPad => lilyPad.color = 'lightgreen');
-  
-//     a
-//   })
-  // Should be a function
-  // Should be able to instantiate a new game
-  // Should report 'game over' if all lives are gone -- should be in game test
-  // Should pause/unpause
-  // Should go to next level when lilypads are all landed on
