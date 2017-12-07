b829e58b065f9a15a4110a8f02923d27e5a586af
Alex Bonder <lexbonder@gmail.com>
2017-12-06 18:59:10 -0700

    Finish first draft tests

--- test/Log-test.js
+++ test/Log-test.js
@@ -27,11 +27,10 @@ describe('Log', function() {
   })
 
   it('Should be able to move', function() {
-    
+    assert.equal(log.x, 10);
+
+    log.drive();
+
+    assert.equal(log.x, 20);
   })
 })
-// Should be function
-// Should be able to make an instance of itself
-// Should be able to take paramaters
-// Should move to left/right
-// Should be able to carry frog
--- test/Vehicle-test.js
+++ test/Vehicle-test.js
@@ -1,4 +1,39 @@
-// Should be function
+const {assert} = require('chai');
+const Vehicle = require('../lib/Vehicle.js')
+const Frog = require('../lib/Frog.js')
+
+describe('Vehicle', function() {
+  let vehicle;
+  
+  beforeEach(function() {
+    vehicle = new Vehicle(10, 10, 10, 10, 10, 'red');
+  })
+
+  it('Should be a function', function() {
+    assert.isFunction(Vehicle);
+  })
+
+  it('Should create an instance of vehicle', function() {
+    assert.isObject(vehicle);
+  })
+
+  it('Should have an X, Y, width, height, speed, and color', function() {
+    assert.equal(vehicle.x, 10)
+    assert.equal(vehicle.y, 10)
+    assert.equal(vehicle.width, 10)
+    assert.equal(vehicle.height, 10)
+    assert.equal(vehicle.dx, 10)
+    assert.equal(vehicle.color, 'red')
+  })
+
+  it('Should be able to move', function() {
+    assert.equal(vehicle.x, 10);
+
+    vehicle.drive();
+
+    assert.equal(vehicle.x, 20);
+  })
+})// Should be function
 // Should be able to make an instance of itself
 // Should be able to take paramaters
 // Should be able to move left/right
