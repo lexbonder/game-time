68bb65136740b24c99b9bbac61b89b1eb8bc8cf4
Alex Bonder <lexbonder@gmail.com>
2017-12-06 17:35:36 -0700

    start log testing

--- test/Log-test.js
+++ test/Log-test.js
@@ -1,3 +1,35 @@
+const {assert} = require('chai');
+const Log = require('../lib/Log.js')
+const Frog = require('../lib/Frog.js')
+
+describe('Log', function() {
+  let log;
+  
+  beforeEach(function() {
+    log = new Log(10, 10, 10, 10, 10, 'brown');
+  })
+
+  it('Should be a function', function() {
+    assert.isFunction(Log);
+  })
+
+  it('Should create an instance of log', function() {
+    assert.isObject(log);
+  })
+
+  it('Should have an X, Y, width, height, speed, and color', function() {
+    assert.equal(log.x, 10)
+    assert.equal(log.y, 10)
+    assert.equal(log.width, 10)
+    assert.equal(log.height, 10)
+    assert.equal(log.dx, 10)
+    assert.equal(log.color, 'brown')
+  })
+
+  it('Should be able to move', function() {
+    
+  })
+})
 // Should be function
 // Should be able to make an instance of itself
 // Should be able to take paramaters
